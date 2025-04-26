d9e385b383e5b5a94e48ece60d712eb314559f0b
Anshbir-Singh <anshbir181@gmail.com>
2025-04-27 02:45:37 +0530

    added controller func

--- controllers/userController.go
+++ controllers/userController.go
@@ -17,10 +17,12 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	models "github.com/Anshbir18/go-jwt/models"
+	"github.com/Anshbir18/go-jwt/helper"
 )
 
 
-var useCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 var validate = validator.New()
 func HashPassword()  {}
 func VerifyPassword()  {}
@@ -28,6 +30,25 @@ func Signup()  {}
 func Login()  {}
 
 //only an admin can access this route and get the users data
-func GetUser()  {} 
 
 func GetUsers()  {}
+func GetUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id":userId}).Decode(&user)
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
