2b97477228b88a877934779f6118f3082e1677c7
Anshbir-Singh <anshbir181@gmail.com>
2025-05-01 00:55:26 +0530

    helper func added

--- controllers/userController.go
+++ controllers/userController.go
@@ -24,7 +24,13 @@ import (
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 var validate = validator.New()
-func HashPassword()  {}
+func HashPassword(password string) string{
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err!=nil{
+		log.Panic(err)
+	}
+	return string(bytes)
+}
 func VerifyPassword(userPassword string , providedPassword string) (bool , string){
 	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
 	check := true
